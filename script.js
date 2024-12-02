1fcea1308738ac837eba66cb598c0441edeb209b
Kalalaw-Bulak <johnpaulartobiedo@gmail.com>
2024-12-02 19:23:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function blue () {
+    let headingID = document.getElementById("changebackground");
+    document.getElementById("blank").innerHTML = "Ben 10 (retroactively known as Classic Ben 10) is an American animated series created by Man of Action, produced by Cartoon Network Studios and distributed by Warner Bros. Domestic Television. The series is about a 10-year-old boy named Ben Tennyson who gets a watch-style alien device called the Omnitrix.";
+    document.body.style.background = ("blue");
+    headingID.classList.remove("image");
+    headingID.classList.remove("tiguwang");
+    headingID.classList.remove("baye");
+    headingID.classList.add("laki");
+}
+
+function pink () {
+    let headingID = document.getElementById("changebackground");
+    document.getElementById("blank").innerHTML = "Sofia the First is an American animated fantasy children's television series created and developed by Craig Gerber for Disney Television Animation and Disney Junior. The series follows a young peasant girl named Sofia, voiced by Ariel Winter, who becomes a princess after her mother marries the King of Enchancia.";
+    document.body.style.background = ("pink");
+    headingID.classList.remove("image");
+    headingID.classList.remove("laki");
+    headingID.classList.remove("tiguwang");
+    headingID.classList.add("baye");
+}
+
+function brown() {
+    let headingID = document.getElementById("changebackground");
+    document.getElementById("blank").innerHTML = "By the U.S. Census's latest numbers, the estimated population is still under 393 million. So yes, there are more guns in the U.S. than there are people. The United States has the highest rate of guns per person, and is the only country with more guns than people.";
+    document.body.style.background = ("brown");
+    headingID.classList.remove("image");
+    headingID.classList.remove("laki");
+    headingID.classList.remove("baye");
+    headingID.classList.add("tiguwang");
+    headingID.classList.add("world");
+}
+
+
